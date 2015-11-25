b04a8732f739ea16b0d0f6003109e4ffcf1173ae
Darren Lee <darren.lee@uts.edu.au>
2015-11-25 19:13:35 +1100

    add launch of konqueror to serve script

--- serve.sh
+++ serve.sh
@@ -3,4 +3,8 @@
 # local python web server on default port (8000)
 #
 
-cd /local/davmdoc/output && python -m pelican.server
+cd /local/davmdoc/output && python -m pelican.server &
+
+sleep 2
+
+konqueror http://127.0.0.1:8000
