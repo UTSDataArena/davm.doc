028ecf0e1907f3f8bafbb1df0dd2c870739d6358
Darren Lee <darren.lee@uts.edu.au>
2016-01-20 17:23:43 +1100

    change to firefox, wait longer to start

--- serve.sh
+++ serve.sh
@@ -5,9 +5,10 @@
 
 cd /local/davmdoc/output && python -m pelican.server &
 
-sleep 2
+sleep 4
 
-konqueror http://127.0.0.1:8000
+# konqueror http://127.0.0.1:8000
+firefox-bin http://127.0.0.1:8000
 
 # this is how you can open another url in a new tab of an existing konqueror
 # qdbus `qdbus | grep -m1 konqueror` /konqueror/MainWindow_1 org.kde.Konqueror.MainWindow.newTab http://uts.edu.au False
